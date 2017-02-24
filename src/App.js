de8c80701d76775a0dfd1502fd9a7b7d1358d043
strongfanfan <463428221@qq.com>
2017-02-24 17:43:45 +0800

    为什么感觉redux好麻烦 - -   写完一个简单的add   回家继续 todolist

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import {addTodo,toggleTodo,setVisibilityFilter,VisibilityFilters} from './action
 import AddTodo from './component/AddTodo'
 class App extends Component {
 
-  render() {
-    const {dispatch,visibleTodos,visibilityFilter}=this.props
-    return (
-     <div>
-       <AddTodo onAddClick={(text)=>
-                    dispatch(addTodo(text))
-       }/>
-     </div>
-    );
-  }
+//   render() {
+//     const {dispatch,visibleTodos,visibilityFilter}=this.props
+//     return (
+//      <div>
+//        <AddTodo onAddClick={text=>
+//                     dispatch(addTodo(text))
+//        }/>
+//      </div>
+//     );
+//   }
+    render(){
+        return (
+            <div>
+                <h2>{this.props.value}</h2>
+                <botton onClick={this.props.onAdd}>add</botton>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import {createStore} from 'redux';
-import { Provider }from 'react-redux'
+import  { Provider }from 'react-redux'
 import todoApp from './reducer/reducer';
+const add=(state=1,action)=> {
+    if (action.type === 'add') {
+        return state + 1
 
-let store = createStore(todoApp);
+    }
+};
+let store = createStore(add);
+const render=()=> {
+    ReactDOM.render(
+        <App value={store.getState()} onAdd={() => store.dispatch({type: 'add'})}/>,
+        document.getElementById('root')
+    );
+};
+render()
+store.subscribe(render)
 
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-  document.getElementById('root')
-);
